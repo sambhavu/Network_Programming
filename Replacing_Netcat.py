d537490580bcefe984f1ce3c931770ca507a844f
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-07-30 19:02:58 -0400

    Update Replacing_Netcat.py

--- Replacing_Netcat.py
+++ Replacing_Netcat.py
@@ -31,6 +31,123 @@ def usage():
     sys.exit(0) 
     
 
+def client_sender(buffer): 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    
+    try: 
+            #connect to our target host 
+        client.connect((target,port))
+        if len(buffer): 
+            client.send(buffer)
+        while True:
+            #now wait for data back 
+            recv_len = 1
+            response = "" 
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data) 
+                response += data
+                if recv_len < 4096:
+                    break
+            print response 
+            #wait for more input 
+            
+            bufffer= raw_input("")
+            buffer += "\n"
+            #send it off 
+            client.send(buffer) 
+            
+    except: 
+        print "[*] Exception! Exiting" 
+        #tear down the connection 
+        client.close()
+        
+        
+        
+                
+def server_loop(): 
+    global target 
+    #if no target is defined, we listen on all interfaces 
+    if not len(target): 
+        target = "0.0.0.0"
+        
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    server.blind((target, port)) 
+    server.listen(5)
+    
+    while True: 
+        client_socket, addr = server.accept() 
+        #spin off a thread to handle our new client 
+        client_thread = threading.Thread(target= client_handler,
+                                         args=(client_socket,))
+        client_thread.start() 
+        
+def run_command(command): 
+    #trim the newline
+    command = command.rstrip()
+    #run the command and get the output back
+    try:
+        output = subprocess.check_output(command,stderr = subprocess.STDOUT, shell = True)
+    except: 
+        output = "Failed to execute command.\r\n" 
+    return output
+    
+    
+def client_handler(client_socket): 
+    global upload
+    global execute
+    global command 
+    
+    #check for upload 
+    if len(upload_destination): 
+        #read in all of the bytes and write to our destination 
+        file_buffer = "" 
+        #keep reading data until none is available 
+        
+        while True: 
+            data = client_socket.recv(1024) 
+            
+            if not data: 
+                break
+            else: 
+                file_buffer += data
+       #now we will take the bytes and try to write them out 
+    try: 
+        file_descriptor = open(upload_destination, "wb") 
+        file_descriptor.write(file_buffer)
+        file_descriptor.close()
+        #Acknoweldge that we wrote all out 
+        client_socket.send("Successfully saved file to %s\r\n" % upload_destination) 
+        
+   except: 
+        client_socket.send("Failed to save file to %s\r\n", upload_destination) 
+        
+    #check for command execution
+    if len(execute):
+        #run the command 
+        output = run_command(execute) 
+        client_socket.send(output) 
+    #now we go into a command shell was requested 
+        if command: 
+            while True: 
+                #show a simple prompt 
+                client_socket.send("<BHP:#>")
+                #now we receive until we see a linefeed (enter key) 
+                
+                cmd_buffer = "" 
+                while "\n" not in cmd_buffer: 
+                    cmd_buffer += client_socket.recv(1024) 
+                    
+                    
+                #send back the command output 
+                response = run_command(cmd_buffer) 
+                
+                #send back the response 
+                client_socket.send(response) 
+                
+             
+    
+    
 def main(): 
     global listen
     global port 
