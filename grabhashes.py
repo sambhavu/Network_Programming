388c1bc6c2edb7fc5dc1f438cb0d97b7b4b8b1ee
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-07-30 23:51:01 -0400

    Update grabhashes.py

--- grabhashes.py
+++ grabhashes.py
@@ -18,4 +18,38 @@ config.LOCATION = "file://%s" memorty_file
 registry.register_global_options(config, commands.Command) 
 registry.register_global_options(config, addrspace.BaseAddressSpace) 
 
+from volatility.plugins.registry.registryapi import RegistryApi
+from volatility.plugins.registry.lsadump import HashDump 
 
+registry = RegistryAPI(config) 
+registry.populate_offsets()
+
+sam_offset = None
+sys_offset = None
+
+for offset in registry.all_offsets: 
+  if registry.all_offsets[offset].endswith("\\SAM"):
+     sam_offset = offset
+     print "[*] SAM: 0x%08x" % offset
+    
+  if registry.all_offsets[offset].endswith("\\system"): 
+     sys_offset = offset
+     print "[*] System: 0x%08x" % offset
+    
+  if sam_offset is not None and sys_ofset is non None: 
+     config.sys_offset = sys_offset
+     config.sam_offset = sam_offset
+    
+    hashdump = HashDump(config) 
+    
+    for hash in hashdump.calculate(): 
+      print hash
+    break 
+  
+  if sam_offset is None or sys_offset is None: 
+    print "[*] Failed to find the system of SAM offsets." 
+    
+    
+    
+    
+    
