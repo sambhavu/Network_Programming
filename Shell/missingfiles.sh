c142543990a26c30496a1f0d36645a14aafe73e2
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-08-25 21:55:25 -0400

    Create missingfiles.sh

---
+++ Shell/missingfiles.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#cmp_dir - program to compare two directories 
+
+#Check for required arguments 
+
+if [ $# -ne 2 ]; then 
+    echo "usage: $0 directory_1 directory_2" 1 > &2
+     exit 1
+     
+fi 
+
+if [ ! -d $1 ]; then 
+    echo "$1 is not a directory!" 1 > &2 
+    exit 1 
+    
+fi 
+
+if [ ! -d $2 ]; then 
+    echo "$2 is not a directory!" 1>&2
+    exit 1
+    
+fi 
+
+missing = 0
+for filename in $1/*; do 
+    fn = $(basename "$filename")
+    if [ -f "$filename" ]; then 
+        if [ ! -f "$2/$fn" ]; then 
+          echo "$fn is missing from $2" 
+          missing = $((missing +1))
+        fi 
+     fi
+     
+done 
+
+echo "$missing files missing" 
+
+
+ 
