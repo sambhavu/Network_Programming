a36916b418ba80b6364e8959cf68cd2725cbe30f
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-08-27 23:37:39 -0400

    Create chmod.sh

---
+++ Shell/chmod.sh
@@ -0,0 +1,4 @@
+chmod 555 scriptname  #gives everyone read/execute permission
+chmod +rx scriptnmae #gives everyone read/execute permission 
+chmod u+rx scriptname #gives only the script owner read/execute permission 
+
