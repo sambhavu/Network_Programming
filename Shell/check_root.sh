02fd9af19c1cf2f58ff2e9bc0b18675bd58c2235
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-09-03 23:04:45 -0400

    Update check_root.sh

--- Shell/check_root.sh
+++ Shell/check_root.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
+
 
 ROOT_UID=0
 
-if[ $UID != ROOT_UID ]
-then 
-echo "You are root" 
+echo $UID
+
+if [ $UID -eq $ROOT_UID ]
+then
+	echo "You are root" 
 else
-echo "You are not root" 
+	echo "You are not root" 
 fi 
+
 exit 0
