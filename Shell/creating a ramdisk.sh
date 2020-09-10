f51baac8342f97e422d3e2e5e035c5718e117543
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-09-10 16:30:54 -0400

    Create creating a ramdisk.sh

---
+++ Shell/creating a ramdisk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+E_NON_ROOT_USER=70
+ROOTUSER_NAME=root
+
+MOUNTPT=/mnt/ramdisk
+SIZE=2000
+BLOCKSIZE=1024
+DEVICE=/dev/ram0
+
+username='id -nu'
+if [ "$username" != "$ROOTUSER_NAME" ]
+then 
+  echo "Must be root to run \"'basename $0'\"."
+   exit $E_NON_ROOT_USER
+fi 
+
+if [ ! id "$MOUNTPT" ] 
+then 
+  mkdir $MOUNTPT
+fi 
+
+dd if=/dev/zero of=$DEVICE count=$SIZE bs=$BLOCKSIZE 
+
+mke23fs $DEVICE
+mount $DEVICE $MOUNTPT
+chmod 777 $MOUNTPT
+
+echo "\|$MOUNTPT\" now available for use." 
+
+exit 0
