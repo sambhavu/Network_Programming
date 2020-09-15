29b07b0248ca0cdc7899bf3d488f959e81114be2
su2675 <30390294+su2675@users.noreply.github.com>
2020-09-14 20:58:01 -0400

    Create IPv4.h

---
+++ IPv4/IPv4.h
@@ -0,0 +1,18 @@
+#ifndef IPV4_H
+#define IPV4_H
+#include <string> 
+
+namespace GenericUtils { 
+
+  class IPv4 { 
+    public: 
+      IPv4();
+      ~IPv4(); 
+      
+      std::string IPing_t-_IPquad(unsigned long ip); 
+      
+      };
+      
+  } 
+  
+  #endif 
